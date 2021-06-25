d8b27b57fb3ce11df2763a180f435c7a360cdcba
Uday Warier <uday.warier1@gmail.com>
2021-06-25 13:01:15 -0400

    Created utility for making new blank processing projects

--- StarterFiles/static/sketch.js
+++ StarterFiles/static/sketch.js
@@ -1,7 +1,8 @@
 function setup() {
     createCanvas(400, 400);
-    text("hello", 200, 200);
   }
   
   function draw() {
+    fill(255, 0, 0);
+    ellipse(100, 100, 40, 40);
   }
---
+++ create_new_project.py
@@ -0,0 +1,5 @@
+import os
+
+os.system('rm -rf ./NewProject')
+os.system('rm -rf ./StarterFiles/__pycache__')
+os.system('cp -R ./StarterFiles ./NewProject')
